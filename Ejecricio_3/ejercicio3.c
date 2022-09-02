919b8eb3e643e9b9153d5ca9a317faa465a5c619
Lucas Pangaro <43672361+lpangaro@users.noreply.github.com>
2022-09-02 17:51:11 -0300

    creacion de archivos para punto 2 y 3

---
+++ Ejecricio_3/ejercicio3.c
@@ -0,0 +1,4 @@
+/* Para compilar: gcc -c -o ejercicio3.o ejercicio3.c -Wall
+ * Para linkear: gcc -o ejercicio3 ejercicio3.o -Wall
+ * Para correr: ./ejercicio3
+ */
---
+++ Ejercicio_2/ejercicio2.c
@@ -0,0 +1,4 @@
+/* Para compilar: gcc -c -o ejercicio2.o ejercicio2.c -Wall
+ * Para linkear: gcc -o ejercicio2 ejercicio2.o -Wall
+ * Para correr: ./ejercicio2
+ */
