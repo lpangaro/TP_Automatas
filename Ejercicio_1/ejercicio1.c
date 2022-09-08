558a7f6ba14a0a14a00731467af4b6f55a1308a6
Lucas Pangaro <43672361+lpangaro@users.noreply.github.com>
2022-09-08 14:54:00 -0300

    Punto 1 OK. Recibe la cadena por archivo o linea de comando, acepta negativo en los decimales, imprime la cantidad de cada tipo de dato

--- Ejercicio_1/ejercicio1.c
+++ Ejercicio_1/ejercicio1.c
@@ -19,76 +19,97 @@ enum ESTADOS{                                                                   
 };
 
 int cantidadElementos(FILE * );
-void lectura_archivo(FILE* arch, int , char*);
-void reconocer(char* valor, FILE* archSalida);
+void reconocer(char* valor, FILE* archSalida, int* cant_oct, int* cant_deci, int* cant_hexa);
 void separarPorComasYReconocer (char*, FILE* salida);
+void ingreso_por_archivo (char*, char*);
 
 int main(){
 
-    int cantidad_elementos = 0;
-
-    FILE *f_entrada = fopen("entrada.txt","r");
+    int menu;
+    char archivo [20];
+    char vector_palabras [1000];
     FILE *f_salida = fopen ("salida.txt", "w");    
 
-    if(f_entrada == NULL){   // si el archivo esta vacio
+    printf("\n\n..:BIENVENIDE AL PROGRAMA DE AUTOMATAS:..\n");
+    printf(" 1) si quiere ingresar la cadena por linea de comandos\n 2) si lo que deasea es ingresar la cadena a traves de un archivo txt \n");
+    
+    printf("Escriba una opcion: ");
+    scanf ("%d", &menu);
+    system("clear");
+
+    switch (menu){
+        case 1 :
+            printf("Ingrese la cadena de caracteres numericos (Hexadecimales, Ocatales o Decimales): ");
+            scanf ("%s", vector_palabras);
+            break;
+        case 2 : 
+            printf("Ingrese el nombre del archivo (ej: entrada.txt): ");
+            //scanf ("%s", archivo);
+            strcpy(archivo, "entrada.txt");
+            ingreso_por_archivo(archivo, vector_palabras);
+            break;
+        default:
+            printf ("No ingreso una opcion valida \n");
+    }
+    
+    separarPorComasYReconocer(vector_palabras, f_salida);                           // PASO EL ARRAY CON LAS PALABRAS A RECONOCER
+    printf("Archivo creado exitosamente. Para visiualizarlo escriba: ´cat salida.txt´\n");
+    
+    fclose (f_salida);
+    return 0;
+}
+
+
+void ingreso_por_archivo (char* archivo, char* vector_palabras) {
+    int i = 0;
+    FILE *f_entrada = fopen(archivo,"r");
+
+    if(f_entrada == NULL){   
         printf("Error en la apertura del archivo.\n");
     }
 
     else {
-        cantidad_elementos = cantidadElementos(f_entrada);
+        fread(&vector_palabras[i], sizeof(char), 1, f_entrada);   //primer elemento
 
-        char* vector_palabras = (char*) calloc(cantidad_elementos + 1, sizeof(char));       // Memoria pedida para la ejecucion del programa.
-
-        lectura_archivo(f_entrada, cantidad_elementos, vector_palabras);                // PASO LOS DATOS DEL TEXTO A UN ARRAY DE CHAR
-
-        separarPorComasYReconocer(vector_palabras, f_salida);                           // PASO EL ARRAY CON LAS PALABRAS A RECONOCER
-        free(vector_palabras);                                                          // Libero espacio de memoria 
-        printf("Archivo creado exitosamente. Para visiualizarlo escriba: ´cat salida.txt´\n");
+        while(!feof(f_entrada)) {
+            i++;
+            fread(&vector_palabras[i], sizeof(char), 1, f_entrada);      //LECTURA DEL ARCHIVO, GUARDO ELEMENTOS DE ENTRADA EN EL VECTOR.
+        }
     }
 
-    
-
     fclose (f_entrada);
-    fclose (f_salida);
-
-    return 0;
 }
 
-// DEVUELVE CANTIDAD DE ELEMENTOS DE MI ARCHIVO
-int cantidadElementos(FILE * f_entrada){                                                     
-    fseek(f_entrada, 0, SEEK_END); //posicion del puntero al final del archivo
-    int tamano = ftell(f_entrada); //ftell retorna la posicion del puntero de lectura
-    fseek(f_entrada, 0, SEEK_SET); //Vuelvo a poner el puntero al inicio
 
-    return tamano/sizeof(char); 
-}
-
-//LEE ARCHIVO Y GUARDA SU CONTENIDO EN UN VECTOR DE CHAR.
-void lectura_archivo(FILE* f_entrada, int cantidad_elementos, char* vector){             
-    fread(vector, sizeof(char), cantidad_elementos, f_entrada);                             //LECTURA DEL ARCHIVO, GUARDO ELEMENTOS DE ENTRADA EN EL VECTOR.
-}
-
-//SEPARA CADA ELEMENTO POR COMA, EVALUA CON EL AUTOMATA Y LO GUARDA JUNTO A SU TIPO EN EL ARCH SALIDA.
+//SEPARA CADA ELEMENTO POR ´&´, EVALUA CON EL AUTOMATA Y LO GUARDA JUNTO A SU TIPO EN EL ARCH SALIDA.
 void separarPorComasYReconocer (char* vector, FILE* f_salida){ 
+    int cant_oct = 0, cant_deci = 0, cant_hexa = 0;
     char* str = NULL;
     const char* sentinela = "&";
+
+    fprintf(f_salida, "Numero                 Cant   Tipo\n");
+
     str = strtok(vector, sentinela);                           // strtok separa el contenido de un vector en palabras 
-    while (str != NULL){                                       // usando el caracter ',' como sentinela. Cada vez que encuentra
-        reconocer(str, f_salida);                                // una ',' envía a reconocer la cadena que estaba detrás de ese caracter
+    while (str != NULL){                                       // usando el caracter '&' como sentinela. Cada vez que encuentra
+        reconocer(str, f_salida, &cant_oct, &cant_deci, &cant_hexa);                              // un '&' envía a reconocer la cadena que estaba detrás de ese caracter
         str = strtok(NULL, sentinela);
     }
 } 
 
 //EVALUA ELEMENTO EN EL AUTOMATA Y LO GUARDA EN EL ARCHIVO SALIDA.
-void reconocer(char* valor, FILE* f_salida){
+void reconocer(char* valor, FILE* f_salida, int* cant_oct, int* cant_deci, int* cant_hexa){
     int estado = INICIO, i = 0;
     while(1){
         switch(estado){
             case INICIO:
                 if(valor[i] == '0')                                                     // ¿El primer caracter es un 0?
                     estado = CONSTANTE_OCTAL1;                                          
-                else if(isdigit(valor[i]))                                              // isdigit evalua si es un caracter numerico.
+                else if(isdigit(valor[i]))                           // isdigit evalua si es un caracter numerico.
                     estado = CONSTANTE_DECIMAL;               
+                else if (valor[i] == '-'){
+                    //i++;
+                    estado = CONSTANTE_DECIMAL;
+                }
                 else
                     estado = DESCONOCIDO;                                               // Si es un primer caracter no numerico es no reconocido.
                 i++;                    
@@ -96,7 +117,8 @@ void reconocer(char* valor, FILE* f_salida){
             
             case CONSTANTE_OCTAL1:
                 if(valor[i] == '\0'){                                                   // ¿FIN (caracter vacio)? 
-                    fprintf(f_salida, "%-20s\tOCTAL\n", valor);
+                    (*cant_oct)++;
+                    fprintf(f_salida, "%-20s\t %d OCTAL\n", valor, *cant_oct);
                     return;
                 }
                 else if(valor[i] == 'x' || valor[i] == 'X')                             // ¿LE SIGUE UNA X? ES HEXADECIMAL
@@ -110,7 +132,8 @@ void reconocer(char* valor, FILE* f_salida){
             
             case CONSTANTE_OCTAL2:
                 if(valor[i] == '\0'){                                                  // ¿FIN? 
-                    fprintf(f_salida, "%-20s\tOCTAL\n", valor);
+                    (*cant_oct)++;
+                    fprintf(f_salida, "%-20s\t %d OCTAL\n", valor, *cant_oct);
                     return;
                 }
                 else if(isdigit(valor[i]) && valor[i] - '0' < 8)
@@ -130,7 +153,8 @@ void reconocer(char* valor, FILE* f_salida){
             
             case CONSTANTE_HEXADECIMAL2:
                 if(valor[i] == '\0'){
-                    fprintf(f_salida, "%-20s\tHEXADECIMAL\n", valor);               // ¿FIN? 
+                    (*cant_hexa)++;
+                    fprintf(f_salida, "%-20s\t %d HEXADECIMAL\n", valor, *cant_hexa);               // ¿FIN? 
                     return;
                 }
                 else if(isxdigit(valor[i]))
@@ -142,7 +166,8 @@ void reconocer(char* valor, FILE* f_salida){
 
             case CONSTANTE_DECIMAL:
                 if(valor[i] == '\0'){
-                    fprintf(f_salida, "%-20s\tDECIMAL\n", valor);                 // ¿FIN? 
+                    (*cant_deci)++;
+                    fprintf(f_salida, "%-20s\t %d DECIMAL\n", valor, *cant_deci);                 // ¿FIN? 
                     return;
                 }
                 else if(isdigit(valor[i]))
